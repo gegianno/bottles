fe89c0c70a0a85a761a3c6c8c68741dec98dd368
Giorgio <gegianno@uc.cl>
2017-06-17 14:06:24 -0400

    Second test passed

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -8,12 +8,10 @@ class BottlesTest < Minitest::Test
 99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.
 VERSE
-    puts ::Bottles.new.verse(89)
     assert_equal expected, ::Bottles.new.verse(99)
   end
 
   def test_another_verse
-    skip
     expected = <<-VERSE
 89 bottles of beer on the wall, 89 bottles of beer.
 Take one down and pass it around, 88 bottles of beer on the wall.
